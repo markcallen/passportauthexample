c093a8ed5bcdc1963f6ae75b7a54a2e4651b56a5
Mark C Allen <mark.allen@hootsuite.com>
2015-03-27 13:18:06 -0700

    Removed unused packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -150,7 +150,6 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-livereload');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-concurrent');
